49b9ec7caf67a050cfcaa979468c617ecc08516f
GeekIllustrated <geekillustrated@Susans-MacBook-Air.local>
2017-01-08 09:45:45 -0800

    added connection to metadata database

--- Simple/views.py
+++ Simple/views.py
@@ -1,13 +1,54 @@
 from Simple import app
 
+#===========================================================================        
+##Import Python Modules Used in App
+#===========================================================================        
+import os, sys, getpass
 from flask import Flask, redirect, url_for, request, session, g,  abort, render_template, flash
 
+from sqlalchemy import create_engine, schema, engine, exc
+from sqlalchemy.engine import reflection
 
-##Define Project Level Variables
-project_name='Simple';
+import pandas as pd
 
-##Define the Views (HTML pages)                                                
+#===========================================================================        
+##Set project level variables
+#===========================================================================        
+project_name = os.getcwd().split('/')[-1]
+user = getpass.getuser()
+dbName = project_name
+dbName= 'GeekIllustratedDOM'
 
+#===========================================================================        
+##Define deg=bugging functions
+#===========================================================================        
+def fatal(*L):
+     print >>sys.stderr, ''.join(L)
+     raise SystemExit
+#===========================================================================        
+##Connect to application metadata database
+#===========================================================================        
+protocol = 'postgresql'
+URL_FORMAT = "%s://%s:%s@%s/%s"
+fname = "/Users/"+user+"/.pgpass"
+f = open(fname, 'rb')
+for a in f.readlines():
+  host, port, database, username, password = a.rstrip().split(':')
+  if (database == dbName):
+      engineURL = URL_FORMAT % (protocol, username, password, host, database)
+  elif (database == '*'):
+      engineURL = URL_FORMAT % (protocol, username, password, host, dbName)
+  else:
+      engineURL = ''
+try:
+   engine = create_engine(engineURL)
+   meta = engine.connect()
+except exc.SQLAlchemyError as detail:
+   print(engineURL)
+   fatal("Could not query : %s" % detail)
+
+#===========================================================================        
+##Define the Functions that Render the Views (HTML pages)                                                
 #===========================================================================        
 #Define the home (index) page with a single slash, and define the page as a render function 
 @app.route('/', methods=['GET', 'POST'])
@@ -18,11 +59,11 @@ def index():
         url = url_for('index');
     else:
         page_title = request.args.get('page_title');
-        url = url_for(page_title);
+        url = url_for('index')+"?page_title="+page_title;
     #Open the web page                                                         
     return render_template('index.html', 
                            project_name = project_name, page_name=page_name, 
                            page_title=page_title,
-                           url = url
+                           url = url, basename=project_name
                            )
 
