9c31df883e38e6a8a98b18f26d9dffa5aea4818e
GeekIllustrated <geekillustrated@Susans-MacBook-Air.local>
2017-01-17 21:17:08 -0800

    Added metadata functions

--- Simple/views.py
+++ Simple/views.py
@@ -32,6 +32,68 @@ dbName = app_name
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 #===========================================================================        
+#  Metadata Functions
+#===========================================================================        
+
+def getPageID():
+    if request.args.get('page_id') is not None:
+        page_id = request.args.get('page_id')
+    elif form.page_id.data is not None:
+        page_id = form.page_id.data
+    else:
+        page_id = 0
+    return page_id
+
+def getPageInfo(page_id):
+    if page_id > 0:
+        psql = "select * from public.page where page_id = %s " % (page_id);
+    else:
+        psql = "select * from public.page order by page_id";    
+    result = conn.execute(psql);
+    fetchall = result.fetchall()
+    pageInfo = pd.DataFrame(fetchall)
+    return pageInfo
+
+def getPageContent(page_id):
+    if (page_id is None) or (page_id == 0) :
+        fatal("getPageContent function requires a numeric page_id argument > 0")
+    csql = "select c.content_id, c.content_md, c.content_ht from public.content c ";
+    csql += "join public.page_content pc on c.content_id = pc.content_id ";
+    csql += "join public.page p on p.page_id = pc.page_id ";
+    csql += "where pc.page_id = %s " % request.args.get('page_id') ;
+    result = conn.execute(psql);
+    fetchall = result.fetchall()
+    pageContent = pd.DataFrame(fetchall)
+    return pageContent
+    
+def postPageContent(page_id, content_id, target):    
+    #Get content that has been submitted via the form
+    new_content_md = form.content_md.data
+    new_content_ht = form.content_ht.data
+    if new_content_md is not None:
+        if (len(new_content_md.strip()) > 0):
+            if (content_id > 0):
+                contsql = "update public.content set "
+                contsql += "content_md = '%s', content_ht = '%s' where content_id = %s " % (new_content_md, new_content_ht, str(content_id)); 
+                #flash(contsql)
+                conn.execute(contsql)
+            else:
+                #flash("Trying to update with " + new_content_md )
+                #flash("Also "+ new_content_ht)
+                contsql = "insert into public.content (content_md, content_ht) VALUES ";
+                contsql += "('%s', '%s')" % (new_content_md, new_content_ht);
+                xsql = "insert into public.page_content "
+                xsql += "(select %s as page_id, max(content_id) as content_id from public.content) " % str(page_id);
+                #flash(contsql)
+                #flash(xsql)
+                #Note: this is not the most ironclad process. I do the insert on the content table, then do the insert on the 
+                #  linking table, using the new content_id that was just created by the insert.   
+                conn.execute(contsql)
+                conn.execute(xsql)
+        #Refresh the page to show the new content
+        return redirect(url_for(target, page_id = page_id ))
+     
+#===========================================================================        
 ##Define the Functions that Render the Views (HTML pages) 
 #===========================================================================        
 #Define the home (index) page with a single slash, and define the page as a render function 
